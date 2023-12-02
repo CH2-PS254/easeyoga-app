4c5d929261448f76e02410e091869fe0d3bc2b39
Ilham Ramadhan <ilhamram332@gmail.com>
2023-12-02 11:54:03 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Capstone_CH2PS254"
+include(":app")
+ 
